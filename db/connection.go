142bcb97d17cf7093b9d2bfb9ea13d4b243b98e3
Felix Enrique Vega Nolasco <49852681+FelixVNolasco@users.noreply.github.com>
2023-04-18 21:01:19 -0600

    db connection

---
+++ db/connection.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DSN = "host=localhost user=postgres password=memento89 dbname=State port=5432 sslmode=disable"
+
+var DB *gorm.DB
+
+func DBConnection() {
+	var error error
+	DB, error = gorm.Open(postgres.Open(DSN), &gorm.Config{})
+
+	if error != nil {
+		log.Fatal(error)
+	} else {
+		log.Print("DB Connected")
+	}
+}
--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"net/http"
 
+	"github.com/FelixVNolasco/real-state-golang/db"
 	"github.com/FelixVNolasco/real-state-golang/routes"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+
+	db.DBConnection()
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", routes.HomeHandler)
