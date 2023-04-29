99ebd1b92d88d0a3c186a01d5657efd0d93de28d
Felix Enrique Vega Nolasco <49852681+FelixVNolasco@users.noreply.github.com>
2023-04-29 16:20:54 -0600

    Added .env file

--- db/connection.go
+++ db/connection.go
@@ -2,21 +2,46 @@ package db
 
 import (
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-var DSN = "host=localhost user=felixvnolasco password=holamundo dbname=state port=5432 sslmode=disable"
+// use os package to get the env variable which is already set
+func envVariable(key string) string {
+
+	// set env variable using os package
+	os.Setenv(key, "gopher")
+
+	// return the env variable using os package
+	return os.Getenv(key)
+}
+
+// var DSN = "host=localhost user=felixvnolasco password=holamundo dbname=state port=5432 sslmode=disable"
 
 var DB *gorm.DB
 
 func DBConnection() {
+
+	// value := envVariable(DSN)
+
+	// Load environment variables from file.
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("failed to load environment variables: %v", err)
+	}
+
 	var error error
-	DB, error = gorm.Open(postgres.Open(DSN), &gorm.Config{})
+	// DB, error = gorm.Open(postgres.Open(DSN), &gorm.Config{})
+	DB, error = gorm.Open(postgres.Open(os.Getenv("DSN")), &gorm.Config{
+		DisableForeignKeyConstraintWhenMigrating: true,
+	})
 
 	if error != nil {
-		log.Fatal(error)
+		// log.Fatal(error)
+		log.Fatalf("failed to connect to PlanetScale: %v", error)
 	} else {
 		log.Print("DB Connected")
 	}
